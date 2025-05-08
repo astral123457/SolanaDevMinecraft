5a48e5ca86557105047bd3c309cbc5e4743149e8
Amauri Bueno Dos Santos <amauri_long@tutamail.com>
2025-05-07 21:26:41 -0300

    Add files via upload

--- app/src/main/java/com/SolanaDevMinecraft/App.java
+++ app/src/main/java/com/SolanaDevMinecraft/App.java
@@ -96,43 +96,7 @@ public boolean onCommand(CommandSender sender, Command command, String label, St
             sender.sendMessage("Este comando só pode ser usado por jogadores.");
         }
         return true;
-    } else if (command.getName().equalsIgnoreCase("testdb")) {
-    if (sender instanceof Player) {
-        Player player = (Player) sender;
-
-        // Obtém as configurações do banco de dados
-        String url = getConfig().getString("database.url");
-        String user = getConfig().getString("database.user");
-        String password = getConfig().getString("database.password");
-
-        // Exibe as configurações para o jogador
-        player.sendMessage("Url: " + url);
-        player.sendMessage("User: " + user);
-        player.sendMessage("Password: " + password);
-
-        try {
-            // Testa a conexão com o banco de dados
-            if (connection == null || connection.isClosed()) {
-                player.sendMessage("Erro: Conexão com o banco de dados não foi estabelecida.");
-                return true;
-            }
-
-            PreparedStatement statement = connection.prepareStatement("SELECT 1");
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                player.sendMessage("Conexão com o banco de dados está funcionando!");
-            } else {
-                player.sendMessage("Erro ao testar a conexão com o banco de dados.");
-            }
-        } catch (Exception e) {
-            player.sendMessage("Erro ao acessar o banco de dados: " + e.getMessage());
-            getLogger().severe("Erro ao testar conexão com o banco de dados: " + e.getMessage());
-        }
-    } else {
-        sender.sendMessage("Este comando só pode ser usado por jogadores.");
-    }
-    return true;
-} else if (command.getName().equalsIgnoreCase("loan")) {
+    } else if (command.getName().equalsIgnoreCase("loan")) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 1) {
@@ -203,25 +167,7 @@ public boolean onCommand(CommandSender sender, Command command, String label, St
         store.buyEmerald(player);
     }
     return true;
-} else if (command.getName().equalsIgnoreCase("buynetheritepickaxe")) {
-    if (sender instanceof Player) {
-        Player player = (Player) sender;
-        store.buyNetheritePickaxe(player);
-    }
-    return true;
-} else if (command.getName().equalsIgnoreCase("buydiamondarmor")) {
-    if (sender instanceof Player) {
-        Player player = (Player) sender;
-        store.buyEnchantedDiamondArmor(player);
-    }
-    return true;
-} else if (command.getName().equalsIgnoreCase("buynetheritearmor")) {
-    if (sender instanceof Player) {
-        Player player = (Player) sender;
-        store.buyEnchantedNetheriteArmor(player);
-    }
-    return true;
-} else if (command.getName().equalsIgnoreCase("soltransfer")) {
+}  else if (command.getName().equalsIgnoreCase("soltransfer")) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 2) {
